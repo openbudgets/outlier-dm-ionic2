0e9908b8254ccbb35e00cfd7ff1bd590f7f9a3cb
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2017-05-10 14:49:50 +0200

    fix: improve UI so fields don't depend on each other

--- src/pages/new-dataset/new-dataset.ts
+++ src/pages/new-dataset/new-dataset.ts
@@ -8,34 +8,29 @@ import { DAMService } from '../../app/services/dam-service';
   providers: [DAMService]
 })
 export class NewDatasetPage {
-  cube: String;
-  factsUri: String;
-  damUrl: String;
+  name: string;
+  cube: string;
+  factsUri: string;
+  damUrl: string;
 
   constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, public alertCtrl: AlertController,
               public damService: DAMService) {
+    this.name = "My New Dataset";
     this.cube = 'aragon-2008-income__568a8';
-    this.factsUri = 'BABBAGE_FACT_URI=http://ws307.math.auth.gr/rudolf/public/api/3/cubes/' + this.cube + '/facts';
-    this.damUrl = 'http://dam-obeu.iais.fraunhofer.de/outlier_detection/LOF/sample?' + this.factsUri;
+    this.factsUri = 'BABBAGE_FACT_URI=http://ws307.math.auth.gr/rudolf/public/api/3/cubes/';
+    this.damUrl = 'http://dam-obeu.iais.fraunhofer.de/outlier_detection/LOF/sample?'
   }
 
-  updateFactsUri(value) {
-    this.factsUri = 'BABBAGE_FACT_URI=http://ws307.math.auth.gr/rudolf/public/api/3/cubes/' + this.cube + '/facts';
-    this.updateDamUrl(value);
-  }
+  startDAMJob(event) {
+    let url = this.damUrl + this.factsUri + this.cube + '/facts';
 
-  updateDamUrl(value) {
-    this.damUrl = 'http://dam-obeu.iais.fraunhofer.de/outlier_detection/LOF/sample?' + this.factsUri;
-  }
-
-  startDAMJob(event, damUrl) {
     let loading = this.loadingCtrl.create({
       content: 'Sending job request to the DAM.'
     });
 
     loading.present();
 
-    this.damService.startDAMJob(damUrl).subscribe(
+    this.damService.startDAMJob(url).subscribe(
       response => {
         loading.dismiss();
         console.log(response);
