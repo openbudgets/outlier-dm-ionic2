fbbcd5454c77e0f2869400a3bc93364db6f40caf
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2017-08-10 17:58:20 +0200

    (build) add configuration files to deploy to heroku

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,3 +1,3 @@
 export class AppSettings {
-    public static API_ENDPOINT='http://127.0.0.1:5000/outlier-dm-webapp/api';
+  public static API_ENDPOINT='https://outlier-api.herokuapp.com/outlier-dm-webapp/api';
 }
