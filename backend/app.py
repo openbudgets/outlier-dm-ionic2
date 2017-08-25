8016c633df43a35a5e376fae088c254b3367c1da
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2017-08-25 17:27:34 +0200

    (build) parse port env var from str to int

--- backend/app.py
+++ backend/app.py
@@ -163,4 +163,4 @@ def api_file_detail(filename):
     return json_answer
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
