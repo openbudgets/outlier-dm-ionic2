7487f51662b0791aa7ae58c480725c44c0cda90a
Piero Hernandez <jeanpierohm@gmail.com>
2017-11-14 15:19:27 +0100

    (frontend) feat: add backend URL for Fraunhofer's server

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,3 +1,5 @@
 export class AppSettings {
-  public static API_ENDPOINT='https://outlier-api.herokuapp.com/outlier-dm-webapp/api';
+  // public static API_ENDPOINT='https://outlier-api.herokuapp.com/outlier-dm-webapp/api';
+  //public static API_ENDPOINT='http://localhost:5000/outlier-dm-webapp/api';
+  public static API_ENDPOINT='http://apps.openbudgets.eu/outlierback/outlier-dm-webapp/api';
 }
